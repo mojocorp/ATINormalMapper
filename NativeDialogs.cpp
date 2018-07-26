edebdc1c625f86e572defbcec78b54484fad8171
Morgan Leborgne <leborgne@zib.de>
2018-07-26 12:09:36 +0200

    Add Cocoa native dialogs

--- NativeDialogs.cpp
+++ NativeDialogs.cpp
@@ -1,5 +1,6 @@
 #include "NativeDialogs.h"
 
+#ifdef _WIN32
 #include <windows.h>
 
 bool
@@ -32,3 +33,48 @@ MessageBox(const char* text, const char* caption, unsigned int uType)
 {
     return MessageBox(NULL, text, caption, uType);
 }
+
+#else
+#ifdef __APPLE__
+#include <Cocoa/Cocoa.h>
+
+bool
+GetOpenFileName(const char* caption, const char* filter, char* filename)
+{
+    NSArray* nsFileTypes = @[ @"nmf", @"tga", @"fs", @"txt" ];
+    NSOpenPanel* panel = [NSOpenPanel openPanel];
+    [panel setMessage:[NSString stringWithCString:caption encoding:NSUTF8StringEncoding]];
+    [panel setAllowsMultipleSelection:NO];
+    [panel setCanChooseDirectories:NO];
+    [panel setCanChooseFiles:YES];
+    [panel setFloatingPanel:YES];
+    [panel setAllowedFileTypes:nsFileTypes];
+    NSInteger result = [panel runModal];
+
+    if (result == NSModalResponseOK) {
+        NSString* nsPath = [panel URLs][0].absoluteString;
+        nsPath = [nsPath stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+        strcpy(filename, [nsPath UTF8String]);
+    }
+    return result == NSModalResponseOK;
+}
+
+int
+MessageBox(const char* text, const char* caption, unsigned int uType)
+{
+    NSAlert* alert = [[[NSAlert alloc] init] autorelease];
+    if (uType & NMB_OK || uType & NMB_OKCANCEL)
+        [alert addButtonWithTitle:@"OK"];
+    if (uType & NMB_OKCANCEL)
+        [alert addButtonWithTitle:@"Cancel"];
+    [alert setMessageText:[NSString stringWithCString:caption encoding:NSUTF8StringEncoding]];
+    [alert setInformativeText:[NSString stringWithCString:text encoding:NSUTF8StringEncoding]];
+
+    NSInteger returnCode = [alert runModal];
+
+    return returnCode == NSAlertFirstButtonReturn;
+}
+#else
+#error "Unsupported platform"
+#endif
+#endif
