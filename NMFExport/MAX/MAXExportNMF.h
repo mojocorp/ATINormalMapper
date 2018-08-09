ad09da6d8a28e5750a500bc41af03c707dc32c19
Morgan Leborgne <leborgne@zib.de>
2018-08-09 21:33:51 +0200

    Move MAX/Maya plugins into a subfolder.

---
+++ NMFExport/MAX/MAXExportNMF.h
@@ -0,0 +1,31 @@
+/**********************************************************************
+ *<
+        FILE: MAXExportNMF.h
+
+        DESCRIPTION:	Template Utility
+
+        CREATED BY:
+
+        HISTORY:
+
+ *>	Copyright (c) 1997, All Rights Reserved.
+ **********************************************************************/
+
+#ifndef __MAXEXPORTNMF__H
+#define __MAXEXPORTNMF__H
+
+#include "Max.h"
+#include "iparamb2.h"
+#include "iparamm2.h"
+#include "istdplug.h"
+#include "resource.h"
+
+#include <commdlg.h>
+#include <direct.h>
+
+extern TCHAR*
+GetString(int id);
+
+extern HINSTANCE hInstance;
+
+#endif // __MAXEXPORTNMF__H
